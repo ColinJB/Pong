dc0f1c7d624d1acdaee4f04ea31359951a9b8e4f
Colin Bloom <Colinjbloom113@gmail.com>
2017-03-08 14:57:03 -0800

    fixed 15 multiplier bug, add css style and img

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,14 @@ var createArray = function(number) {
   for(var i = 1; i <= number; i++) {
     var replace = "";
 
-    if (i % 3 === 0) {
-      replace = "ping";
+    if (i % 15 === 0) {
+      replace = "ping-pong";
       playerArray.push(replace);
     } else if (i % 5 === 0) {
       replace = "pong";
       playerArray.push(replace);
-    } else if (i % 15 === 0) {
-      replace = "ping-pong";
+    } else if (i % 3 === 0) {
+      replace = "ping";
       playerArray.push(replace);
     } else {
       playerArray.push(i);
@@ -29,14 +29,12 @@ $(document).ready(function() {
     var playerInput = $("#inputNum").val();
     var playerNum = parseInt(playerInput);
 
-    console.log(createArray(playerNum));
-
     var newArray = createArray(playerNum);
 
     var numString = newArray.join(",");
 
     $("#output").text(numString);
-    $("#output").show();
+    $("#output").fadeIn();
     event.preventDefault();
   });
 });
